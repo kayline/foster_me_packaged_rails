2a6596d04a4a5705f6c2db7207139558d7e80b29
Molly Trombley-McCann <molly.kayline@gmail.com>
2018-08-22 14:34:13 -0700

    users can log in with linksgit stgit stgit st

--- client/src/shared/SplashPageHeader.js
+++ client/src/shared/SplashPageHeader.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../assets/paw_logo_white.svg';
-import { Header } from 'semantic-ui-react'
+import { Header, Button } from 'semantic-ui-react'
 
 class SplashPageHeader extends Component {
 	render() {
@@ -8,6 +8,8 @@ class SplashPageHeader extends Component {
 			<header className="App-header-splash">
 	      <img src={logo} className="App-logo-splash" alt="logo" />
 	      <Header size="huge" className="App-title">Welcome to FosterMe</Header>
+	      <Button><a href="/users/sign_in">Sign In</a></Button>
+	      <Button><a href="/users/sign_out">Sign Out</a></Button>
     	</header>
 			)
 	}
