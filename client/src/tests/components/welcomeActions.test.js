7ed8c6dca68652f966b36e1dc00f9912db0dddf0
Molly Trombley-McCann <molly.kayline@gmail.com>
2018-08-25 16:32:47 -0700

    Use enzyme for all js tests

--- client/src/tests/components/welcomeActions.test.js
+++ client/src/tests/components/welcomeActions.test.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
 import WelcomeActions from '../../components/WelcomeActions.js'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<WelcomeActions />, div);
+  shallow(<WelcomeActions />)
 });
