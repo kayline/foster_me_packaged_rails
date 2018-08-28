d7c1857a3c451e2b5ceece620339b2e7d54f4876
Molly Trombley-McCann <molly.kayline@gmail.com>
2018-08-28 13:00:04 -0700

    separate layout for devise views
    Remove notice messages, they are confusing as part of React Flow
    I don not understand why the application layout was getting in my react views, but it was

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
+	layout :layout_by_resource
 
 	def fallback_index_html
     render :file => 'public/index.html'
+  end
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
   end
 end
