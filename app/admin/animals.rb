6e34a181acfe6d1c3bb32202345bac8fb7280b40
Molly Trombley-McCann <molly.kayline@gmail.com>
2018-09-04 20:14:06 -0700

    Allow admins to edit image_urls

--- app/admin/animals.rb
+++ app/admin/animals.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register Animal do
-	permit_params :name, :description, :sex, :date_of_birth, :foster_family_id
+	permit_params :name, :description, :sex, :date_of_birth, :foster_family_id, :image_url
 end
