ac6e625bf83aac1d3f09b16f1f209a211164663f
Molly Trombley-McCann <molly.kayline@gmail.com>
2018-09-07 13:30:12 -0700

    File name input can be clicked, does not appear disabled

--- client/src/components/animals/AnimalFormFields.js
+++ client/src/components/animals/AnimalFormFields.js
@@ -44,8 +44,8 @@ class AnimalFormFields extends Component {
 		    <Form.Input onChange={this.onFieldChange} name="name" className="animal-name" label="Animal Name" placeholder='Animal Name' />
 		    <Form.TextArea onChange={this.onFieldChange} label="Description" placeholder='What does this critter look like?' name="description" className="animal-description"/>
 	    	<Form.Select onChange={this.onFieldChange} label="Sex" options={sexOptions} placeholder="Sex" name="sex" className="animal-sex" />
-	    	<Form.Field onChange={this.handleDOBFieldChange} label="Date of Birth" control={DatePicker} selected={this.state.animal.date_of_birth} className="animal-date-of-birth" name="date_of_birth"/>
 	    	<FileUploadFormField onChange={this.handlePhotoUpload} label="Profile Photo" placeholder="Upload a Photo" />
+	    	<Form.Field onChange={this.handleDOBFieldChange} label="Date of Birth" control={DatePicker} selected={this.state.animal.date_of_birth} className="animal-date-of-birth" name="date_of_birth"/>
 	    </div>
 		)
 	}
--- client/src/shared/FileUploadFormField.js
+++ client/src/shared/FileUploadFormField.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Icon } from 'semantic-ui-react'
+import { Icon } from 'semantic-ui-react'
 
 class FileUploadFormField extends Component {
 	constructor(props) {
@@ -48,9 +48,9 @@ class FileUploadFormField extends Component {
 			<div className="field file-upload">
 		 		<label>{this.props.label}</label>
 		 		<input type="file" ref="fileInput" className="file-input" onChange={this.onFileSelected} style={{display: 'none'}} />
-			 	<div className="ui action input fileInput">
+			 	<div className="ui action input file-input-trigger" onClick={this.openFileSelection}>
 				 	<input type="text" className="file-name" disabled="true" placeholder={this.state.placeholder} value={this.state.filename} />
-	        <label className="ui icon button btn-file select-file-trigger" onClick={this.openFileSelection}>
+	        <label className="ui icon button btn-file select-file-trigger">
              <Icon name='attach' />
 	        </label>
 			 	</div>
